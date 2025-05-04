71af0f41ace77f0d7e237e8083db73fa22312e8c
Vladyslav <154080171+metimol@users.noreply.github.com>
2025-05-04 15:44:04 +0200

    Add basePath and assetPrefix for production environment; create .nojekyll file

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
     images: {
         unoptimized: true,  // Disable image optimization for static export
     },
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 };
 
 export default nextConfig;
